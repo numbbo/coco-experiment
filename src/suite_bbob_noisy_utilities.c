bdbe047e947dd857bb4233e3786d4b273b697148
Olaf Mersmann <olafm@p-value.net>
2024-08-15 01:40:28 +0200

    [core] Fix naming of random seeds

--- src/suite_bbob_noisy_utilities.c
+++ src/suite_bbob_noisy_utilities.c
@@ -15,17 +15,17 @@
  * @name Methods and global variables needed to perform random sampling
  */
 /**{@*/
-static long _RANDNSEED = 30; /** < @brief Random seed for sampling Uniform noise*/
-static long _RANDSEED = 30;  /** < @brief Random seed for sampling Gaussian noise*/
+static long RANDSEED = 30; /** < @brief Random seed for sampling Uniform noise*/
+static long RANDNSEED = 30;  /** < @brief Random seed for sampling Gaussian noise*/
 
 /**
  * @brief Increases the normal random seed by one unit
  * Needed to sample different values at each time
  */
 void increase_random_n_seed(void){
-  _RANDNSEED += 1;
-  if (_RANDNSEED > (long) 1.0e9)
-    _RANDNSEED = 1;
+  RANDNSEED += 1;
+  if (RANDNSEED > (long) 1.0e9)
+    RANDNSEED = 1;
 }
 
 /**
@@ -33,17 +33,17 @@ void increase_random_n_seed(void){
  * Needed to sample different values at each time
  */
 void increase_random_seed(void){
-  _RANDSEED += 1;
-  if (_RANDSEED > (long) 1.0e9)
-    _RANDSEED = 1;
+  RANDSEED += 1;
+  if (RANDSEED > (long) 1.0e9)
+    RANDSEED = 1;
 }
 
 /**
  * @brief Resets both random seeds to the initial values
  */
 void coco_reset_seeds(void){
-  _RANDSEED = 30;
-  _RANDNSEED = 30;
+  RANDSEED = 30;
+  RANDNSEED = 30;
 }
 
 /**
@@ -55,7 +55,7 @@ double coco_sample_gaussian_noise(void){
   double gaussian_noise;
   double gaussian_noise_ptr[1] = {0.0};
   increase_random_n_seed();
-  bbob2009_gauss(&gaussian_noise_ptr[0], 1, _RANDNSEED);
+  bbob2009_gauss(&gaussian_noise_ptr[0], 1, RANDNSEED);
   gaussian_noise = gaussian_noise_ptr[0];
   return gaussian_noise;
 }
@@ -70,7 +70,7 @@ double coco_sample_uniform_noise(void){
   double uniform_noise_term;
   double noise_vector[1] = {0.0};
   increase_random_seed();
-  bbob2009_unif(&noise_vector[0], 1, _RANDSEED);
+  bbob2009_unif(&noise_vector[0], 1, RANDSEED);
   uniform_noise_term = noise_vector[0];
   return uniform_noise_term;
 }
