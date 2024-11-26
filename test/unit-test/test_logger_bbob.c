144882afea5c6d31e3b3a47151cc95f2d647b334
Tea Tušar <tea.tusar@ijs.si>
2024-11-26 01:32:57 +0100

    Added an additional test to see that the logger outputs final values.

--- test/unit-test/test_logger_bbob.c
+++ test/unit-test/test_logger_bbob.c
@@ -1,6 +1,28 @@
 #include "minunit.h"
 #include "coco.c"
 #include "about_equal.h"
+#include <stdio.h>
+
+
+/**
+ * Counts the number of lines in a file.
+ */
+int count_lines_in_file(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        coco_error("Error opening file %s", filename);
+        return -1; 
+    }
+    int lines = 0;
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            lines++;
+        }
+    }
+    fclose(file);
+    return lines;
+}
 
 /**
  * Tests the logarithmic and linear triggers via the bbob logger.
@@ -142,10 +164,71 @@ MU_TEST(test_logger_bbob_triggers) {
 }
 
 /**
+ * Test whether the last line is correctly printed to the various log files. 
+ */
+MU_TEST(test_logger_bbob_last_line) {
+
+  coco_suite_t *suite;
+  coco_observer_t *observer;
+  coco_problem_t *problem;
+
+  double *x = coco_allocate_vector(2);
+  double *y = coco_allocate_vector(1);
+
+  int i = 0;
+  char *filename, *folder;
+
+  suite = coco_suite("bbob", "", "dimensions: 2 function_indices: 1 instance_indices: 1");
+  observer = coco_observer("bbob", "number_evaluation_triggers: 4 base_evaluation_triggers: 100");
+  /* Use the 2-D sphere function */
+  problem = coco_suite_get_next_problem(suite, observer);
+  folder = coco_strdup(observer->result_folder);
+
+  x[0] = 0; x[1] = 0;
+  coco_evaluate_function(problem, x, y);
+  coco_recommend_solution(problem, x);
+  mu_check(about_equal_value(y[0], 80.88209408));
+
+  x[0] = 0; x[1] = -0.5;
+  coco_evaluate_function(problem, x, y);
+  mu_check(about_equal_value(y[0], 79.97529408));
+
+  x[0] = 0.2; x[1] = -1.1;
+  coco_evaluate_function(problem, x, y);
+  mu_check(about_equal_value(y[0], 79.48601408));
+
+  for (i = 0; i < 5; i++) {
+    x[0] = 0; x[1] = 0;
+    coco_evaluate_function(problem, x, y);
+  }
+
+  coco_observer_free(observer);
+  coco_suite_free(suite);
+
+  filename = coco_strdupf("%s%s%s%s%s", folder, coco_path_separator, "data_f1", 
+    coco_path_separator, "bbobexp_f1_DIM2.dat");
+  mu_check(count_lines_in_file(filename) == 5);
+  coco_free_memory(filename);
+
+  filename = coco_strdupf("%s%s%s%s%s", folder, coco_path_separator, "data_f1", 
+    coco_path_separator, "bbobexp_f1_DIM2.tdat");
+  mu_check(count_lines_in_file(filename) == 9);
+  coco_free_memory(filename);
+
+  filename = coco_strdupf("%s%s%s%s%s", folder, coco_path_separator, "data_f1", 
+    coco_path_separator, "bbobexp_f1_DIM2.mdat");
+  mu_check(count_lines_in_file(filename) == 3);
+  coco_free_memory(filename);
+
+}
+
+
+/**
  * Run all tests in this file.
  */
 int main(void) {
   MU_RUN_TEST(test_logger_bbob_triggers);
+  MU_RUN_TEST(test_logger_bbob_last_line);
 
   MU_REPORT();
 
