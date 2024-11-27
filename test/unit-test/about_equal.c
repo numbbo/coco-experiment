bacd7e4dd4148084f10ff96609dfb3b3f6fe441c
Olaf Mersmann <olafm@p-value.net>
2024-11-27 14:50:11 +0100

    [unit-test] Output absolute and relative error when about_equal fails

--- test/unit-test/about_equal.c
+++ test/unit-test/about_equal.c
@@ -1,25 +1,18 @@
 #include "about_equal.h"
 
+#include <stdio.h>
 #include <math.h>
 
 int about_equal_value(const double a, const double b) {
-  /* Shortcut to avoid the case where a - b is tiny and both a and b
-   * are close to or equal to 0.
-   *
-   * Example: a = +EPS and b = -EPS then the relative error is 2 but
-   * in fact the two numbers are both for all practical purposes 0.
-   */
-  if (a == b) {
-    return 1;
-  } else {
-    const double absolute_error = fabs(a - b);
-    const double larger = fabs(a) > fabs(b) ? a : b;
-    const double relative_error = fabs((a - b) / (fabs(larger) + 1e-23));
+  const double absolute_error = fabs(a - b);
+  const double larger = fabs(a) > fabs(b) ? a : b;
+  const double relative_error = fabs((a - b) / (fabs(larger) + 1e-23));
 
-    if (absolute_error < 1e-13)
-      return 1;
-    return relative_error < 4e-6;
+  if (a == b || absolute_error < 1e-13 || relative_error < 4e-6) {
+    return 1;
   }
+  fprintf(stderr, "ERROR: about_equal_value failed: %f != %f (abs_err=%f, rel_err=%f)\n", a, b, absolute_error, relative_error);
+  return 0;
 }
 
 int about_equal_vector(const double *a, const double *b, const size_t dimension) {
@@ -32,7 +25,6 @@ int about_equal_vector(const double *a, const double *b, const size_t dimension)
   return 1;
 }
 
-
 int about_equal_2d(const double *a, const double b1, const double b2) {
-  return (about_equal_value(a[0], b1) && about_equal_value(a[1], b2));
+  return about_equal_value(a[0], b1) && about_equal_value(a[1], b2);
 }
