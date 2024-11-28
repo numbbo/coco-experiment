29244ab1cf6b31ee4973811f0748f3b1ad503aa8
Olaf Mersmann <olafm@p-value.net>
2024-11-28 20:36:33 +0100

    [ci] Use locally built wheels
    Python was not picking up the locally built wheels and instead using the
    version published on pypi. Make sure pip installs the local wheel. This
    uncovered another issue where we were testing on platform combinations that are
    not supported on Github CI (namely Ubuntu / musllinux and Windows 2019 / win32).

--- build/python/setup.py
+++ build/python/setup.py
@@ -1,6 +1,7 @@
 import numpy as np
 from setuptools import Extension, setup
 
+
 extensions = []
 extensions.append(Extension(name="cocoex.interface",
                             sources=["src/cocoex/coco.c", "src/cocoex/interface.pyx"],
